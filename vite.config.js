824c2b549a05289954300afa5083d33a6f964940
Sue Smith <sue@benormal.info>
2025-05-14 16:21:14 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    build: {
+      outDir: "_deploy/_site",
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+          404: '404.html',
+        },
+      }
+    }
+  }
+});
